69920899d334417ebd4ec53242a6d84174726740
Marc A. Saegesser <marc@saegesser.org>
2020-12-17 19:32:52 -0600

    Day 17 -- A more general solution

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,12 +1,75 @@
 package advent
 
+/** A general n-dimeninsional Conway Game of Live
+  *
+  * It's a little slower than the games for specific dimensions but
+  * works for any n.
+  */
 object Day17 {
   def run(): Unit = {
-    println(s"Day17.part1 = ${Day17_1.part1()}")
-    println(s"Day17.part2 = ${Day17_2.part2()}")
+    println(s"Day17.part1 = ${part1(readFile(3, inputFile))}")
+    println(s"Day17.part2 = ${part2(readFile(4, inputFile))}")
   }
+
+  type Space = Set[CoordN]
+
+  case class CoordN(xs: Vector[Int]) {
+    def apply(is: Iterable[Int]): CoordN = CoordN(is.toVector)
+
+    def adjacent = {
+      (0 until xs.size).foldLeft(Set(this)) { case (a, i) =>
+        a.flatMap { x => (x.xs(i)-1 to x.xs(i)+1).map(xx => CoordN(x.xs.updated(i, xx))) }
+      } - this
+    }
+  }
+
+  def part1(space: Space): Int =
+    runN(6, space).size
+
+  def part2(space: Space): Int =
+    runN(6, space).size
+
+  def reachable(space: Space): Set[CoordN] =
+    space.foldLeft(Set.empty[CoordN]) { case (a, c) => a ++ c.adjacent }
+
+  def applyRules(c: CoordN, space: Space): Boolean = {
+    (space(c), (c.adjacent & space).size) match {
+      case (true, sz) if Set(2, 3).contains(sz) => true
+      case (true, _)                            => false
+      case (false, sz) if sz == 3               => true
+      case (false, _)                           => false
+    }
+  }
+
+  def step(space: Space): Space = {
+    reachable(space).filter(applyRules(_, space))
+  }
+
+  def runN(n: Int, space: Space): Space =
+    if(n == 0) space
+    else       runN(n-1, step(space))
+
+  def parseInput(dim: Int, lines: Iterator[String]): Space = {
+    val pad = Vector.fill(dim - 2)(0)
+
+    def parseLine(y: Int, line: String): Set[CoordN] =
+      line.zipWithIndex.collect { case (c, x) if c == '#' => CoordN(Vector(x, y) ++ pad) }.toSet
+
+    lines.zipWithIndex.foldLeft(Set.empty[CoordN]) { case (s, (l, y)) => s ++ parseLine(y, l) }
+  }
+
+  def readFile(dim: Int, f: String): Space =
+    parseInput(dim, io.Source.fromFile(f).getLines())
+
+
+  val inputFile = "data/Day17.txt"
+
+  def testData = """.#.
+                   |..#
+                   |###""".stripMargin.linesIterator
 }
 
+// A 4 dimeninsional game of life
 object Day17_2 {
 
   def part2(): Int = {
@@ -61,6 +124,7 @@ object Day17_2 {
                    |###""".stripMargin.linesIterator
 }
 
+// A 3 dimeninsional game of life
 object Day17_1 {
 
   def part1(): Int =
